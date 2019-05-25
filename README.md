31120dc56b4178b97857117c896b3c56195e3e4a
Ruben te Wierik <r.e.c.tewierik@student.tudelft.nl>
2019-05-25 22:06:42 +0200

    Modified main.py file to launch a basic application supported by Kivy. Added data folder. Updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Text-Adventure-Game
 This is a collaborative text adventure game!
+
+## Installation instructions
+Installation instructions here.
--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-# This is a commment.
+# Backbone of the application.
+import kivy
+kivy.require('1.10.1')
+
+# Required modules of the "kivy"-framework.
+from kivy.app import App
+from kivy.uix.label import Label
+
+# Class representing the application. Everything that happens in the application is controlled from here.
+class TextAdventure(App):
+
+    # Builds the graphical user interface of the application.
+    def build(self):
+        return Label(text='Hello world')
+
+# Runs the application.
+TextAdventure().run()
